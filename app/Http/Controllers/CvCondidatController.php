2c4df3d775d5321d6fdcf5c1aef87eb7891ce8ac
KhadidjaDari <58103719+KhadidjaDari@users.noreply.github.com>
2019-12-22 10:54:13 +0100

    le candidat peut pas creer un cv avant inserer les information personelle

--- app/Http/Controllers/CvCondidatController.php
+++ app/Http/Controllers/CvCondidatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Cv;
+use App\Condidat;
 class CvCondidatController extends Controller
 {
     //
@@ -15,11 +16,18 @@ class CvCondidatController extends Controller
         return view('cv.AfficheinfoCv');
     }
     public function titre_mon_cv(Request $request){
+        $cond=Condidat::select('id')->where('user_id','=',Auth::user()->id)->get();
+        if($cond!='[]'){
      $titre_cv=new Cv();
      $titre_cv->user_id=Auth::user()->id;
      $titre_cv->titre=$request->input('titre');
      $titre_cv->save();
      return redirect('Cv_Condidat') ;
+    }
+    else{
+        echo "<script>alert('il faut inserer votre information personel avant creation de cv')</script>";
+        return view('profil.InserCondidat');
+    }
     }
     public function Afficher_Info_Cv(){
         return view('cv.AfficheinfoCv');
